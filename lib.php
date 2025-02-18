1468b344c25c4d43d07a50a8ccaacf55847bd551
Mark Sharp <mark.sharp@solent.ac.uk>
2025-02-18 16:17:15 +0000

    Return true for PHPStan

--- lib.php
+++ lib.php
@@ -63,4 +63,5 @@ function local_customurls_pluginfile($course, $cm, $context, $filearea, $args, $
 
     // We can now send the file back to the browser - in this case with a cache lifetime of 1 day and no filtering.
     send_stored_file($file, 86400, 0, $forcedownload, $options);
+    return true;
 }
